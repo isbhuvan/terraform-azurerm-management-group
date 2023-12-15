e9b7004cd8774dbc590295129484314171fe37f2
isBhuvan <isBhuvan@outlook.com>
2023-12-15 11:11:34 +0530

    moved parent MG data source to main

--- data.tf
+++
@@ -1,4 +0,0 @@
-data "azurerm_management_group" "parent_management_group" {
-  count = var.parent_management_group_name != null ? 1 : 0
-  name  = var.parent_management_group_name
-}
--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+data "azurerm_management_group" "parent_management_group" {
+  count = var.parent_management_group_name != null ? 1 : 0
+  name  = var.parent_management_group_name
+}
+
 resource "azurerm_management_group" "management_group" {
   name                       = var.name
   display_name               = var.display_name != null ? var.display_name : var.name
